53793a7b4c83a477d723a7dba1358306a9a30d8a
drdung1999 <duongdung12a8@gmail.com>
2020-03-16 13:04:49 +0700

    [1.0.0] handle cancel request contact real time

--- src/socket/index.js
+++ src/socket/index.js
@@ -1,11 +1,13 @@
 import addNewContact from './contactSocket/addNewContact';
 import {userSocket} from './userSocket/userSocket';
-import acceptContact from './contactSocket/acceptContact'
+import acceptContact from './contactSocket/acceptContact';
+import cancelReqContactSend from './contactSocket/cancelReqContactSend';
 
 let initSocket = (io) => {
   addNewContact(io);
   userSocket(io);
   acceptContact(io);
+  cancelReqContactSend(io);
 }
 
 module.exports = initSocket;
